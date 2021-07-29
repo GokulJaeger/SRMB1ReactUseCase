7cbbdfe0c95cbb8b2541c4561be636e8856f8b8f
GokulJaeger <gokuljaeger@gmail.com>
2021-07-29 21:21:31 +0530

    server updated

---
+++ inventorymanagementspringboot/src/main/java/com/example/inventorymanagement/inventorymanagementspringboot/InventorymanagementspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagement.inventorymanagementspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagementspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagementspringbootApplication.class, args);
+	}
+
+}
